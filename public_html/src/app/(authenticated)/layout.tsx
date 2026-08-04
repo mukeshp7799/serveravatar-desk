a89d0f99ad3beeca12d16043581b81102db596b5
mukeshp7799 <mukesh@serveravatar.com>
2026-08-04 12:24:25 +0000

    Added zoom-in page animation (animate-page-zoom-in) on all authenticated pages

--- public_html/src/app/(authenticated)/layout.tsx
+++ public_html/src/app/(authenticated)/layout.tsx
@@ -439,7 +439,7 @@ export default function AuthenticatedLayout({ children }: { children: React.Reac
 
         <main className="flex-1 min-w-0 overflow-hidden h-[calc(100vh-4rem)]">
           <Scroll containerClassName="h-full" className="h-full" watch={pathname}>
-            <div className="p-4 sm:p-6 w-full animate-fade-in-up">{children}</div>
+            <div key={pathname} className="p-4 sm:p-6 w-full animate-page-zoom-in">{children}</div>
           </Scroll>
         </main>
       </div>
